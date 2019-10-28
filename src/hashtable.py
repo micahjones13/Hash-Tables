1374dfdc538f96500ae9df648b8423524ac0dca1
micahjones13 <micahjones13@gmail.com>
2019-10-28 16:53:22 -0600

    day 1 finished

--- src/hashtable.py
+++ src/hashtable.py
@@ -1,21 +1,27 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"{self.value}"
+
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -25,7 +31,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -34,14 +39,13 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
+        # returns index int
         return self._hash(key) % self.capacity
-
 
     def insert(self, key, value):
         '''
@@ -51,9 +55,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        pair = LinkedPair(key, value)
+        hash_key = self._hash_mod(key)
 
-
+        # if exceeds storage, ressize
+        if self.capacity >= len(self.storage):
+            self.resize()
+    # insert wherever the hash index is
+        if self.storage[hash_key] is None:
+            self.storage[hash_key] = pair
+        #!Handle collisions tomorrow
+        #! Need something for if the key already exits
 
     def remove(self, key):
         '''
@@ -63,8 +75,14 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        hash_key = self._hash_mod(key)
+        # print(hash_key, 'hash_key')
+        # print(key, 'key')
+        if self.storage[hash_key] != None:
+            # print(self.storage[hash_key], 'self.stoarge[hash_key]')
+            self.storage[hash_key] = None
+        else:
+            print('Key not found')
 
     def retrieve(self, key):
         '''
@@ -74,8 +92,13 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        hash_key = self._hash_mod(key)
 
+        if self.storage[hash_key] is not None:
+            # print(self.storage[hash_key], 'retrieved')
+            return self.storage[hash_key]
+        else:
+            return None
 
     def resize(self):
         '''
@@ -87,31 +110,60 @@ class HashTable:
         pass
 
 
+# if __name__ == "__main__":
+#     ht = HashTable(2)
 
-if __name__ == "__main__":
-    ht = HashTable(2)
+#     ht.insert("line_1", "Tiny hash table")
+#     ht.insert("line_2", "Filled beyond capacity")
+#     ht.insert("line_3", "Linked list saves the day!")
 
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+#     print("")
 
-    print("")
+#     # Test storing beyond capacity
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
 
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+#     # Test resizing
+#     old_capacity = len(ht.storage)
+#     ht.resize()
+#     new_capacity = len(ht.storage)
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+#     print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+#     # Test if data intact after resizing
+#     print(ht.retrieve("line_1"))
+#     print(ht.retrieve("line_2"))
+#     print(ht.retrieve("line_3"))
+#     print("")
+# ht1 = HashTable(2)
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+# print(ht1.storage, 'BEFORE INSERT')
+# print(ht1._hash_mod('a'))
+# print(ht1._hash_mod('z'))
 
-    print("")
+# ht1.insert('a', 0)
+# ht1.insert('b', 2)
+
+# ht1.remove('a')
+# ht1.retrieve('a')
+
+# print(ht1.storage)
+ht = HashTable(8)
+
+ht.insert("key-0", "val-0")
+ht.insert("key-1", "val-1")
+ht.insert("key-2", "val-2")
+ht.insert("key-3", "val-3")
+ht.insert("key-4", "val-4")
+ht.insert("key-5", "val-5")
+ht.insert("key-6", "val-6")
+ht.insert("key-7", "val-7")
+ht.insert("key-8", "val-8")
+# ht.insert("key-9", "val-9")
+
+print(ht.retrieve('key-0'))
+print(ht.storage)
+ht.remove('key-0')
+
+print(ht.storage)
